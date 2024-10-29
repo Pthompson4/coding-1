9f2829df47eb70dfbacac9efc72d227a3742df3c
Pthompson4 <thompsond700@boyslatin.net>
2024-10-29 19:15:16 +0000

    first day of learning about java script, strings, double quotes, integers, and single quotes. (notes)

---
+++ unit 2/jsPractice.js
@@ -0,0 +1,21 @@
+//Data types are the basic building 
+//block of Java Script programs
+
+//Strings
+//strings are a piece of data that is wrapped in
+//quotation marks. 
+
+"double quotes tell the computer that this is a string."
+"My name is Pierre"
+"309090"
+"1!#$!#$"
+'single quotes can also be used to write strings.'
+console.log('my name is Pierre');
+
+// integers
+//integers are pieces of data that represent numbers.
+23
+345
+1023
+32009472095702970
+console.log(3*12)
