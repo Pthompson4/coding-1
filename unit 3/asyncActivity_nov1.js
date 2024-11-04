22fd4719e867f432b5a091062f5e0566a2b9fdaf
Pthompson4 <thompsond700@boyslatin.net>
2024-11-04 19:00:04 +0000

    async activity nov 1

---
+++ unit 3/asyncActivity_nov1.js
@@ -0,0 +1,6 @@
+// var declares a variable, let declares re-assignable local variables, and const specifies that a variables value is constant. 
+let a = 2000
+let b = 1000
+let c = 1000
+// you could use the let variable and const variable 
+// 
