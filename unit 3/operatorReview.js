9916dd88680a2a5ab068c91ab8766a741b815a79
Pthompson4 <thompsond700@boyslatin.net>
2024-11-13 20:14:53 +0000

    operator review 11/13

---
+++ unit 3/operatorReview.js
@@ -0,0 +1,32 @@
+//Arithmetic -writing code expressions that reform math operations
+
+// examples of apps that use arithmetic operators
+
+// depositong money inyo a digital account
+
+var depositeAmount = 50
+var checkingAccount = 50
+var depositeAccount = 50
+
+console.log(depositeAmount + checkingAccount)
+
+//function- set of instructions that prefrorm a job
+
+//peer to peer money transfers
+var amountBeingSent = "";
+var userAccount = "";
+
+
+
+//Assigment- giving variables values.
+//we use the single equal sign (=) to assign data to variables
+
+//snaekers is the variable.  we are assuming the value of 200.00 with the equal sign.
+var sneakers = 200.00
+sneakers *= .30
+console.log (sneakers)
+
+//Comparison- analyzing  values. checking to see if a certain codition is met
+
+
+//Logical
