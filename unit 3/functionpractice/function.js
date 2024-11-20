710543dcb4c86288758c20359f0cdb20ebebcc7e
Pthompson4 <thompsond700@boyslatin.net>
2024-11-20 20:15:15 +0000

    function.js practice changes made

---
+++ unit 3/functionpractice/function.js
@@ -0,0 +1,33 @@
+// what is a function
+'A function is a set of intructions that performs a specific task or job.'
+
+// you have two variations of functions
+// built in functions 
+// users defined functions 
+// built-in functions- These are instructions that are pre-written
+//into the coding language,All we need to do is pass in data.
+
+//exmples
+var classname = 'coding class'
+console.log(classname)
+
+// user deffined functions are coustoms intructions written by enginers to make code that performs specific task
+  // how to write user defined functions
+
+  function examplefunction(){
+    console.log('the function script is connected')
+  }
+  // in order to have a function work. we need to call it
+  // the above code is the function definition-meaning
+  // its only telling the computer the instruction - not to actually run it.
+
+  // we acess or call a function by writing its name.
+  examplefunction()
+
+
+  let accountBalance = 1000;
+  
+ function depositMoney () {
+    let newBalance = accountBalance + 200;
+    document.getElementById("data").innerHTML = newBalance;
+ }
