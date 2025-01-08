40ec8ec3f4524a4c99d07321b797166f7094ce45
Pthompson4 <thompsond700@boyslatin.net>
2025-01-08 20:25:57 +0000

    q2 quiz jan 8 2025

---
+++ Quarter2_Quiz.js
@@ -0,0 +1,62 @@
+//1. What is an integer data type? Please write your response in a complete sentence as a string data type.
+"integers are data types that represent numbers."
+
+//2. In your own words, describe what an if/ else conditional statement is. Please write your response in a complete sentence as a string data type.
+" a if/ else coditional statement is a tool that is used to create specific outcomes based on recieved data from the function."
+
+//3. In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+"a function is a set of instructions that work to prefrorm a job"
+
+//4. What is a boolean data type? Please write your response in a complete sentence as a string data type.
+" a boolean data type is a tool that helps us figure out what is true of false"
+
+//5. In your own words, describe the difference between a function parameter and a function argument. Please write your response in a complete sentenc as a string data type.
+"function Parameters act as variables inside the method, When a function parameter is passed to the method, it is called a function argument"
+//6. Which operator would work best for the following function: An digital locker function that needs to have a specific set of numbers to be entered in order to 
+// lock and unlock a locker in a gym. Please write your answer as a string.
+"logical"
+
+//7. Which operator would work best for the following function: A height verification function that needs to check if a user is a certain height to get onto an amusement
+//  park ride. Please write your answer as a string.
+"comparison"
+
+//8. You have been hired as an engineer to develop a sales tax function for the state of Pennsylvania. They would like to be able to take in a annual ammount of money
+//  businesses in the state make and deduct the proper amount of taxes from the their earnings. The state of PA should take 7% of a companies earnings.
+//  Your function should take in a total sales amount as an argument and return the amount thats owed to the state.
+
+//Provided is an example of what your program should do:
+
+//If Ian's Development and Design company made $200,000.00, the state should deduct 7% from that total.
+// Ian's Development and Design company should pay $14,000.00 in sales tax.
+
+
+
+
+
+
+
+
+
+//9. You have been hired by Apple to help them fix their password system. They would like you to create A function that will check how long a password is.
+//  Your program should be able to accept an integer password and also check if the password is more or less than 10 characters long. 
+// if the password is longer than 10 characters, your program should tell the user that their password is too long. 
+// If the password is under 4 characters the function should tell the user the password is too short. If the password meets the length criteria, 
+// there should be a message confirming to the user that their password was created successfully.
+// HINT - you will need to use a function that counts the number of characters in a string. I recommend looking this up on W3schools.
+
+//10. You have applied to several colleges in the state of PA and as your final project, you must create a function that will inform you on whether you will be
+//  admitted into the school based on the schools' reccomendation letter criteria. Your function should take in the name of the school along with whether it 
+// is TRUE or FALSE that you recieved a reccomendation letter from one of the staff members at Boys Latin.
+// Provided are the schools you've applied to and their reccomendation letter criteria.
+
+// Temple Univesity - So long as you have 1 reccomendation letter from BL, you will get in.
+//Penn State - You must have 2 letters of recomendation from BL to get in.
+//University of Pennsylvania - You must have 2 letter from BL to get in.
+//Cheyney University - You must have 1 letter of reccomendation from BL to get in.
+//Provided are the letters of reccomendations you've recieved:
+
+//Both Mr. Parker and Mr. Kimble have given you a reccomendation letter to Cheyney University.
+//Mr. Kimble has given you a letter of reccomendation for Penn State, but Mr. Parker and Mr. Paris have not.
+//Mr. Paris has given you a letter of reccomendation to Temple University, but Mr. Parker and Mr. Kimble have not.
+//Mr. Parker has given you a letter of reccomnedation to The University of Pennsylvania, Mr. Kimble did not give you letter of reccomendation, But Mr. Paris did.
+//Your function should return which schools you did or did not get into based on the information above.
