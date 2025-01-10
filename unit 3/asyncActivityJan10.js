6a5111837f7b3aeb4efca7e744fee7917b5aa7d0
Pthompson4 <thompsond700@boyslatin.net>
2025-01-10 20:29:24 +0000

    async activity  jan 10 2024

---
+++ unit 3/asyncActivityJan10.js
@@ -0,0 +1,28 @@
+//1. Create 3 different lists data types that will include things found in grocery store. Make sure your items use the string data type.
+
+
++
+// Create a list that includes 5 items that you can find in the frozen food isle.  
+["Frozen pizza", "frozen vegetables", "frozen fruit", "frozen waffles", "ice cream"]
+// Create a list that includes 5 items that you can find in the international food isle.
+["kimchi", "ramen", "sushi", "taco shells","siracha"]
+// Create a list that includes 5 items that you can find in the produce isle.
+["apples", "bananas","grapes", "celery", "tomatoes"]
+
+
+
+//2. With the list provided below; use the print (for python) or console.log (for javascript)
+//  functions to return the correct value for the index.
+numbers = [1,30,39,50,293,100]
+
+Hint: If you get stuck, search for accessing JS list by index or accessing Python
+ list by index depending on which language you are learning
+
+print/ console.log the number at index 0
+print/ console.log the number at index 3
+print/ console.log the number at index 4
+
+
+
+
+//3. Create a function that will multiply the number at index 4 in the list above.
