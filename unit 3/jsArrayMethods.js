05bea09927f8f0aba647f4cefc592dfc678d5cc3
Pthompson4 <thompsond700@boyslatin.net>
2025-01-15 20:17:19 +0000

    js array methods 1/15/25

---
+++ unit 3/jsArrayMethods.js
@@ -0,0 +1,31 @@
+//in python / java they have list, dict, set, tuple which are all data types that\
+
+
+
+
+
+var name = "Pierre";
+var grocery = ["apple", "milk", "water"];
+var, let, const, name 
+
+// an array can store multiple data types
+
+var generalist= [name1, grocery, 132323, 23.34, true];
+
+// array methods:
+
+// a method is a function that works on a list (or object). 
+//methoods are written after the list variable name and declared with a period. 
+//pretty much; the method is going insiode of list and running instructions on data from the list
+console.log() - method 
+console.error()
+
+generalist.push('new data')
+console.log(generalist)
+
+// pop method() - this method removes a piece of data from a list it'll remove the last item in that list 
+generalist.pop()
+console.log(generalist)
+
+//length() method - this method counts the number of data inside of a list
+console.log(generalist.length)
