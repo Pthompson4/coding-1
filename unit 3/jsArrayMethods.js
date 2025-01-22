cc72235e872f339dc5d0f1a793992e5d5052e4b5
Pthompson4 <thompsond700@boyslatin.net>
2025-01-22 20:18:42 +0000

    jan 22 work

--- unit 3/jsArrayMethods.js
+++ unit 3/jsArrayMethods.js
@@ -20,12 +20,14 @@ var generalist= [name1, grocery, 132323, 23.34, true];
 console.log() - method 
 console.error()
 
+//push method () - pushing new data out into an array. push needs an argument to be passed out into the array
 generalist.push('new data')
 console.log(generalist)
 
-// pop method() - this method removes a piece of data from a list it'll remove the last item in that list 
+// pop method() - this method removes a piece of data from a list it'll remove the last item in that list. pop does not need an arguement
+// it'll always remove
 generalist.pop()
 console.log(generalist)
-
+//LIFO= last in, First out
 //length() method - this method counts the number of data inside of a list
 console.log(generalist.length)
---
+++ unit 4/whileLoops.js
@@ -0,0 +1,17 @@
+//Loops = a special construct that repeats a set of code instructions over and ove under specific conditions
+
+// while loops = a special type of loop where so long as a condition is true it will repeat the code instructions over and over
+
+var x = 2 // this is an ex of an assignment operator - giving a variable a value 
+
+// = : assignment operator
+// == : comparison operator (same as)
+// ! = : comparison operator (not the same as)
+
+while (x = 2){
+    consolelog('this message will repeat forever.')
+}
+
+
+
+
