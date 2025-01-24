2f01a2de0b386d8018c39104daaa55b85ab4c12b
Pthompson4 <thompsond700@boyslatin.net>
2025-01-24 22:41:28 +0000

    Jan 24 async activity.

---
+++ unit 4/asyncActivityJan24.js
@@ -0,0 +1,24 @@
+//1. numberList = [1,23,56,3,56,3,20,200]
+
+//Create a function that uses a while loop to print/ console.log the list but in reverse order.
+
+function reverse [1,23,56,3,56,3,20,200] {
+    var reverse = 200, 20, 3, 56, 3, 23,1
+    while (var reverse 1,23,56,3,56,3,20,200){
+        console.log ('reverse' + interval)
+        
+    }
+    console.log("reverse!")
+}
+
+//2. Create a countdown timer that takes in an number passed in from a HTML document. Your HTML document should be 
+// called countdownLoop.html your countdown timer function should use a while loop
+//  that will start the count based on the number you provide and then stop at zero (for example; 
+// If I type in the number 5 my countdown timer should output 5,4,3,2,1,0 in the console.log). 
+// The numbers counting down should be displayed in the your browsers console (right-click to open the inspect tool 
+// and the hover over the icon that says console.)
+// Provided below is the html template you can use for your code. The template already has the input field written 
+// and will output your entry in the console. You need to finish this code by adding your countdown function. 
+
+// you can write you function between the <script> tags OR you can use the src 
+// attribute and import your js file into your html file.
